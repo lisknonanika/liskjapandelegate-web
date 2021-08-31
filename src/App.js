f7db9a2540fda713f47318b5297af466dfeec28c
lisknonanika <lisknonanika@gmail.com>
2021-08-31 15:24:34 +0900

    add

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>Delegate liskjapan</div>
+      </header>
+      <div className="App-contents">
+        a
+      </div>
+    </div>
+  );
+}
+
+export default App;
